59c24f54fd6aa23e6bdc8f8fd8c30b0a84d61f9a
Jed garza <Jedgarza@gmail.com>
2019-09-16 23:57:19 -0500

    Added more questions, edditited the quesions and radiobuttons css, and added submit button

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,235 +1,74 @@
-$('#start').on('click',function(){
-   for(var i =0; i < questions.length; i++){
-      $('#subwrapper').apend('<h2>' + questions[i].question+ '</h2>');\
-      for(var j = 0; j < questions[i].answers.length; i++){
-         $('#subwrapper').apend('<input type="radio" name="question-" + i + ')
-      }
-   }
-})
 
+var correct = 0;
+var wrong = 0;
+var unAnswered = 0;
+var timeLeft = 0;
+
+function startGame(){
 var questions = [{
    question: 'Who is Luke Skywalkers dad?',
-   answers:['Obi-wan , Darth Maul , Anakin , Yoda'],
+   answers:["Obi-wan","Darth Maul","Anakin","Yoda"],
    correctAnswer: 'Anakin'
    }, {
    question: 'Who killed Mace Windu?',
-   answers:['Jango-Fet , Jaba the Hutt , Count Dooku , Darth Vader'],
+   answers:['Jango-Fet' , 'Jaba the Hutt' , 'Count Dooku' , 'Darth Vader'],
    correctAnswer: 'Darth Vader'
    }, {
    question: 'What planet does Yoda go into excile on?',
-   answers:['Taatoie , Degaboth , Corasont , YavinIV'],
+   answers:['Taatoie' , 'Degaboth' , 'Corasont' , 'YavinIV'],
    correctAnswer: 'Degaboth'
-   }];
+   },{
+      question: 'Who killed the first Sith in a thousand years?',
+      answers:['Qui-Gon Jinn' , 'Yoda' , 'Luke Skywalker' , 'Obi-wan'],
+      correctAnswer: 'Obi-wan'
+   }, {
+      question: 'What is the name of Han Solo\'s ship',
+      answers:['Soulless One' , 'Death Star' , 'Millennium Falcon' , 'Eclipse'],
+      correctAnswer: 'Millennium Falcon'
+      }, {
+      question: 'What was the name of Darth Vaders flag ship?',
+      answers:['Eclipse' , 'The Executor' , 'The vigilance' , 'The Devastator'],
+      correctAnswer: 'The Executor'
+      }];
+
+$('#start').on('click',function(){
+  
+   for(var i =0; i < questions.length; i++){
+      $('#subwrapper').append('<br><br><h2>' + questions[i].question+ '</h2>');
+      for(var j = 0; j < questions[i].answers.length; j++){
+         
+         $('#subwrapper').append("<input id='radioCss' type='radio' name='question-"+ i +"' value='"+questions[i].answers[j]+"' >"+questions[i].answers[j]);
+       console.log(questions[i].answers[j]);
+      }
+   }
+})
+}
+
+function endOfGame(){
+   
+   for(var k = 0; k < questions.length; k++){
+      var rabioButtons = document.getElementsByName('questions-'+ k);
+      for(var a = 0; a < rabioButtons.length; a++){
+      if (radioButtons[a].checked){
+      if(radioButtons[a].value == answers[k]){
+      correct++
+      } else{
+      wrong++
+      }
+      }
+      }}
+
+     unAnswered =  questions.length - (wrong + right);
+        
+   }
+ 
+ 
+   $('#submit').on('click', function(){ 
+      endOfGame();
+      return unAnswered;
+       
+     })
+startGame();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var questions=["Who is Luke Skywalkers father?","", "What planet does Yoda go into excile on?"];
-var choices=["Obi-wan , Darth Maul , Anakin , Yoda","Jango-Fet , Jaba the Hutt , Count Dooku , Darth Vader","Taatoie , Degaboth , Corasont , YavinIV"];
-var answers=["Anakin","Darth Vader","Degaboth"];
-var choicelist;
-
-
-//var addToDiv="<form>";
-// Loop through each question in our array and make up the multiple choice form for that question
-//for(var i=0;i<questions.length;i++){
-   //addToDiv=addToDiv+"<br><br>";
-//addToDiv+=questions[i]+"<br>";
-//var choicelist=choices[i].split(", ");
-//for(var c=0;c<choicelist.length;c++){
-//addToDiv+="<input type='radio' name="+i+" value="+choicelist[c]+">"+choicelist[c];
-//addToDiv+="<input type='hidden' name=Answer For "+i+" value="+answers[i]+">";
-//}
-
-// This displays the right answer on the screen..**********************************
-//document.getElementById("questionDiv").innerHTML=addToDiv;
-//}
-// Now that we are done creating multiple choice questions, create a Submit button and close the form tag
-//document.getElementById("questionDiv").append="<BR><BR><input type='Submit'></form>";
-//
-//*********************************************************************************************
-//*********************************************************************************************
-//*********************************************************************************************
-
-
-// var trivia = [{
-//      questions:{
-//          q1: 'Who is Luke Skywalkers Father?',
-//          q2: 'Who killed Mace Windu?'
-//      },
-//      choices:{
-//          q1: ['Obi-Wan', 'Darth Sidious', 'Darth maul', 'Anakin'],
-//          q2: ['Darth Vader', 'Darth Malgus', 'Master Yoda', 'Captn Rex']
-//      },
-//      answers:{
-//          q1: 'Anakin',
-//          q2: 'Darth Vader'
-//      }
-//     }]
-
-// console.log(trivia.questions.q1);
-// console.log(trivia.choices.q1);
-// console.log(trivia.answers.q1);
-// console.log(trivia.questions.q2);
-// console.log(trivia.choices.q2[2]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var trivia = {
-//      questions:{
-//          q1: 'Who is Luke Skywalkers Father?',
-//          q2: 'Who killed Mace Windu?'
-//      },
-//      choices:{
-//          q1: ['Obi-Wan', 'Darth Sidious', 'Darth maul', 'Anakin'],
-//          q2: ['Darth Vader', 'Darth Malgus', 'Master Yoda', 'Captn Rex']
-//      },
-//      answers:{
-//          q1: 'Anakin',
-//          q2: 'Darth Vader'
-//      }
-//     }
-
-// console.log(trivia.questions.q1);
-// console.log(trivia.choices.q1);
-// console.log(trivia.answers.q1);
-// console.log(trivia.questions.q2);
-// console.log(trivia.choices.q2[2]);
